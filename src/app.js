4c3143c80d0693d9ba7f7fc1454969ec1e190095
Mellang <dlwpgh23@naver.com>
2021-12-27 00:08:08 +0900

    create project and apply es6

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get(`/`, (req, res, next) => {
+  res.send("Hello!!");
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`listening port ${port}`);
+});
