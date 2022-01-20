62d9e7394a92b2a0cb739b060cfe234f66a74f08
Mellang <dlwpgh23@naver.com>
2022-01-20 21:08:14 +0900

    fix userSchema

--- src/Model/userSchema.ts
+++ src/Model/userSchema.ts
@@ -15,6 +15,7 @@ export default function userModel() {
     password: { type: String, default:null },
     user_name: { type: String ,default: null},
     follow: { type: [String], default: [] ,ref:"User"},
+    group:{type:[String] , default:[] , ref:"Group"},
     user_img: { type: String, default: null },
     created_at: { type: Date, default: null },
     updated_at: { type: Date, default: null },
