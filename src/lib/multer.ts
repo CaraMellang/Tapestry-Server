8619e7acee62ace937208dce67110cfba856d255
Mellang <dlwpgh23@naver.com>
2022-02-28 17:22:05 +0900

    not encrypt image file

--- src/lib/multer.ts
+++ src/lib/multer.ts
@@ -1,8 +1,10 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
 import aws from "aws-sdk";
+import {v4} from "uuid";
 import dotenv from "dotenv";
 dotenv.config();
+
 
 const s3 = new aws.S3({
   credentials: {
@@ -17,10 +19,14 @@ const s3 = new aws.S3({
 const multerUpload = multerS3({
   s3: s3,
   bucket: "tapestry-image-bucket/tapestry/images", // 둘중에 뭐가  폴더 형성인지;; 일단 얘를 따르긴해
+  contentType: multerS3.AUTO_CONTENT_TYPE,
   acl: "public-read",
+  key: (req, file, cb) => {
+    console.log("파일정보(multer.ts)", file);
+    cb(null, v4() + file.originalname);
+  },
 });
 
 export const uploadImage = multer({
-  dest: "tapestry/images", // 둘중에 뭐가  폴더 형성인지;;
   storage: multerUpload,
 });
