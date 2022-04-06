0cce189bca7340c47b7fb791865ff73aaff6b1ef
Mellang <dlwpgh23@naver.com>
2022-04-07 00:28:40 +0900

    add profile image patch

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
 import jwt from "../lib/jwt";
+import { s3, uploadProfile } from "../lib/multer";
 import validTokenMiddleware from "../lib/validTokenMiddleware";
 import { UserModel } from "../Model/RootModel";
 
@@ -39,6 +40,38 @@ profileRouter.patch(
       res
         .status(500)
         .send({ status: 500, message: "에러가 발생했습니다.", err });
+    }
+  }
+);
+profileRouter.patch(
+  `/setimage`,
+  validTokenMiddleware,
+  uploadProfile.single("profile_img"),
+  async (req: Request, res, next) => {
+    let imageFile: any = req.file;
+    try {
+      const findUser = await UserModel.findOne({
+        email: res.locals.user.email,
+      });
+      const { user_img }: { user_img: string } = findUser;
+      if (user_img) {
+        await s3
+          .deleteObject({
+            Bucket: "tapestry-image-bucket",
+            Key: `tapestry/profile/${decodeURIComponent(
+              user_img.split("tapestry/profile/")[1]
+            )}`,
+          })
+          .promise();
+      }
+      await UserModel.updateOne(
+        { email: res.locals.user.email },
+        { user_img: imageFile.location }
+      );
+      return res.status(201).send({ status: 201, message: "업데이트 성공" });
+    } catch (err) {
+      res.status(500).send({ status: 500, message: "Failed", err });
+      next(err);
     }
   }
 );
